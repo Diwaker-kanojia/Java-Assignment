161ffbacd7419162e9c0df037f9ebc6923abde7c
Diwaker kanojia <diwakerkanojia17@gmail.com>
2025-07-25 19:49:44 +0530

    AreaOfRectangle

---
+++ Basic/Area.java
@@ -0,0 +1,10 @@
+public class Area {
+    public static void main(String[] args) {
+        int length = 4;
+        int breadth = 10;
+
+        int areaOfRectangle = length * breadth;
+
+        System.out.println("Area = " + areaOfRectangle);
+    }
+}
