5d0a150b6cc454573c865411092041e791e36deb
Diwaker kanojia <diwakerkanojia17@gmail.com>
2025-07-26 23:23:04 +0530

    VowelOrConsonants

---
+++ ConditionBased/VowelOrConsonant.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class VowelOrConsonant {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a letter : ");
+        char mychar = sc.nextLine().charAt(0);
+
+        switch (mychar) {
+            case 'a':
+                System.out.println("Vowel");
+                break;
+            case 'e':
+                System.out.println("Vowel");
+                break;
+            case 'i':
+                System.out.println("Vowel");
+                break;
+            case 'o':
+                System.out.println("Vowel");
+                break;
+            case 'u':
+                System.out.println("Vowel");
+                break;
+            case 'A':
+                System.out.println("Vowel");
+                break;
+            case 'E':
+                System.out.println("Vowel");
+                break;
+            case 'I':
+                System.out.println("Vowel");
+                break;
+            case 'O':
+                System.out.println("Vowel");
+                break;
+            case 'U':
+                System.out.println("Vowel");
+                break;
+            default:
+                System.out.println("Consonants");
+                break;
+        }
+    }
+}
