dc671e3d65f9ad311c59947b809085ec0cabc912
Diwaker kanojia <diwakerkanojia17@gmail.com>
2025-08-01 12:25:36 +0530

    Series2

---
+++ LoopBased/series/Series2.java
@@ -0,0 +1,8 @@
+public class Series2 {
+    public static void main(String[] args) {
+        for (int number = 9; number > 0; number--) {
+            System.out.print(number * (number + 2) + " ");
+        }
+
+    }
+}
