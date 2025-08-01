3108b90d831b97474c22e3d53e0b356526a38bf8
Diwaker kanojia <diwakerkanojia17@gmail.com>
2025-08-01 12:57:41 +0530

    Series5

---
+++ LoopBased/series/Series5.java
@@ -0,0 +1,11 @@
+public class Series5 {
+    public static void printLetter() {
+        for (char letter = 'A'; letter <= 'Z'; letter++) {
+            System.out.print(letter + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        printLetter();
+    }
+}
