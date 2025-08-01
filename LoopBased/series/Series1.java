4bb28107d9a7c44f455871e7d9db79d5d1259f07
Diwaker kanojia <diwakerkanojia17@gmail.com>
2025-08-01 12:15:24 +0530

    Series1

---
+++ LoopBased/series/Series1.java
@@ -0,0 +1,9 @@
+public class Series1 {
+    public static void main(String[] args) {
+        for (int number = 10; number >= 1; number--) {
+            if (number != 8 && number != 4) {
+                System.out.print(number + " ");
+            }
+        }
+    }
+}
