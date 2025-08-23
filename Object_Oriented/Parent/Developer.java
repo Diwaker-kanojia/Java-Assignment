c47691ac7829f1dd1cab03f382567f78df98bb89
Diwaker-kanojia <diwakerkanojia17@gmail.com>
2025-08-23 21:42:05 +0530

    Employee

---
+++ Object_Oriented/Parent/Developer.java
@@ -0,0 +1,7 @@
+package Parent;
+
+public class Developer extends Employee {
+    protected void work() {
+        System.out.println("Developer is writing code");
+    }
+}
---
+++ Object_Oriented/Parent/Employee.java
@@ -0,0 +1,7 @@
+package Parent;
+
+public class Employee {
+    protected void work() {
+        System.out.println("Employee is working.");
+    }
+}
