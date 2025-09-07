2ffaf3e8518d6f42b81f321bf392dc505dc0a327
Diwaker kanojia <diwakerkanojia17@gmail.com>
2025-09-07 22:20:56 +0530

    CharAt

--- Strings/MyString.java
+++ Strings/MyString.java
@@ -1,4 +1,4 @@
-public class MyString extends Object {
+public class MyString {
     public final char[] value;
 
     public MyString(String str) {
@@ -12,14 +12,26 @@ public class MyString extends Object {
 
     // override toString Method
     public String toString() {
-        return "to String method";
+        return new String(value);
     }
 
-    // to find the index of any character
+    // to find the character of given index
     public char charAt(int index) {
-        if (index > value.length) {
+        if (index < 0 || index >= value.length) {
             return ' ';
         }
         return value[index];
     }
+
+    // to find the index of character
+    public int indexOf(char ch) {
+        for (int i = 0; i < value.length; i++) {
+            if (value[i] == ch) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    //
 }
